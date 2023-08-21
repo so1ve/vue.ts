db93f5b52df28ae7303b25da63bf4e3594d989ab
so1ve <i@mk1.io>
2023-08-21 19:42:08 +0800

    tests: fix

--- test/index.test.ts
+++ test/index.test.ts
@@ -5,10 +5,15 @@ import { describe, expect, it } from "vitest";
 import { transform } from "../src/core/transform";
 import AVue from "./__fixtures__/a.vue?raw";
 
+const normalizeContent = (content: string) => content.replace(/\r\n/g, "\n");
+
 describe("should", () => {
 	it("exported", () => {
 		expect(
-			transform(AVue, join(__dirname, "__fixtures__", "a.vue")),
+			transform(
+				normalizeContent(AVue),
+				join(__dirname, "__fixtures__", "a.vue"),
+			),
 		).toMatchSnapshot();
 	});
 });
