df8d0a521df0d79d9cacd6e516b24595b0bd6f75
so1ve <i@mk1.io>
2023-11-04 15:49:33 +0800

    test: add tests for vuejs/core#8468

---
+++ test/__fixtures__/core#8468-props.ts
@@ -0,0 +1,4 @@
+type Text = { type: "text"; value: string };
+type Number = { type: "number"; value: number };
+
+export type Inputs = Text | Number;
---
+++ test/__fixtures__/core#8468.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts" generic="P extends Inputs">
+import type { Inputs } from "./core#8468-props.ts";
+
+defineProps<P>();
+</script>
