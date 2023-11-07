fc5466f5fb71a0c081d4dc97960b3134d61f0b20
so1ve <i@mk1.io>
2023-11-07 15:08:30 +0800

    test: add tests for vuejs/core#9541

---
+++ test/__fixtures__/defineEmits/core#9541.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+interface Base {
+	(e: "change", id: number): void;
+}
+interface Emits extends Base {
+	(e: "unChange", id: number): void;
+}
+const emit = defineEmits<Emits>();
+
+emit("change", 1);
+</script>
