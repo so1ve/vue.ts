507626acfa89b7d40877e52692aafc8a27d89891
so1ve <i@mk1.io>
2023-11-08 14:13:24 +0800

    refactor: reuse `ValidTransforms`

--- src/core/types.ts
+++ src/core/types.ts
@@ -2,13 +2,13 @@ import type MagicString from "magic-string";
 
 import type { Printer } from "./printer";
 
-export interface Options {
-	tsconfigPath?: string;
-	defineEmits?: boolean;
-	defineProps?: boolean;
-}
+export type ValidTransforms = "defineEmits" | "defineProps";
 
-type ValidTransforms = "defineEmits" | "defineProps";
+export type Options = {
+	tsconfigPath?: string;
+} & {
+	[Transform in ValidTransforms]?: boolean;
+};
 
 export type ResolvedOptions = Required<Options>;
 export type TransformOptions = Pick<ResolvedOptions, ValidTransforms>;
