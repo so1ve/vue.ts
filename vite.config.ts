3836766537064401984d1e484c763a80759c9b84
so1ve <i@mk1.io>
2023-11-28 21:23:59 +0800

    test: increase timeout

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import TsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-	test: {},
+	test: { testTimeout: 50_000 },
 	plugins: [TsconfigPaths()],
 });
