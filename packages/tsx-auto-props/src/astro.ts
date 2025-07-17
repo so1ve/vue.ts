53ef4b710822e18c3ed919905ba9b7de057eabbe
so1ve <i@mk1.io>
2025-07-17 16:03:17 +0800

    rename

--- packages/tsx-auto-props/src/astro.ts
+++ packages/tsx-auto-props/src/astro.ts
@@ -2,7 +2,7 @@ import type { Options } from "./types";
 import vite from "./vite";
 
 export default (options: Options) => ({
-	name: "@vue.ts/complex-types",
+	name: "@vue.ts/tsx-auto-props",
 	hooks: {
 		"astro:config:setup": async (astro: any) => {
 			astro.config.vite.plugins ??= [];
