099f0b513d3021452e55c97463f0ae759a1586fe
so1ve <i@mk1.io>
2025-07-20 13:24:15 +0800

    feat(complex-types): use `defu` to merge config

--- packages/complex-types/src/core/utils.ts
+++ packages/complex-types/src/core/utils.ts
@@ -1,14 +1,19 @@
 import { join } from "node:path";
 
+import { defu } from "defu";
+
 import type { Options, ResolvedOptions } from "./types";
 
-export const resolveOptions = (rawOptions: Options): ResolvedOptions => ({
-	include: rawOptions.include ?? ["**/*.vue"],
-	exclude: rawOptions.exclude ?? ["node_modules/**"],
-	tsconfigPath: rawOptions.tsconfigPath ?? join(process.cwd(), "tsconfig.json"),
-	defineEmits: rawOptions.defineEmits ?? true,
-	defineProps: rawOptions.defineProps ?? true,
-});
+const defaultOptions: ResolvedOptions = {
+	include: ["**/*.vue"],
+	exclude: ["node_modules/**"],
+	tsconfigPath: join(process.cwd(), "tsconfig.json"),
+	defineEmits: true,
+	defineProps: true,
+};
+
+export const resolveOptions = (rawOptions: Options) =>
+	defu(rawOptions, defaultOptions) as ResolvedOptions;
 
 const quotesReg = /"/g;
 export const escapeQuotes = (s: string) => s.replace(quotesReg, '\\"');
