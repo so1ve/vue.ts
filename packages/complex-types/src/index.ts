3085737822e8064cb516ffacd4908881e29d7363
so1ve <i@mk1.io>
2025-10-31 16:08:03 +0800

    fix(complex-types): make it work during HMR

--- packages/complex-types/src/index.ts
+++ packages/complex-types/src/index.ts
@@ -1,4 +1,4 @@
-import { ensureLanguage } from "@vue.ts/language";
+import { ensureLanguage, getLanguage } from "@vue.ts/language";
 import { createFilter } from "@vue.ts/shared";
 import type { UnpluginFactory } from "unplugin";
 import { createUnplugin } from "unplugin";
@@ -6,6 +6,8 @@ import { createUnplugin } from "unplugin";
 import { transform } from "./core/transform";
 import type { Options } from "./core/types";
 import { resolveOptions } from "./core/utils";
+
+const languageExtRegexp = /\.((?:c|m)?(?:j|t)sx?|d\.ts|vue)$/i;
 
 export const unpluginFactory: UnpluginFactory<Options | undefined> = (
 	options = {},
@@ -16,6 +18,48 @@ export const unpluginFactory: UnpluginFactory<Options | undefined> = (
 	return {
 		name: "@vue.ts/complex-types",
 		enforce: "pre",
+
+		vite: {
+			async handleHotUpdate(ctx) {
+				const { file } = ctx;
+				if (!languageExtRegexp.test(file)) {
+					return;
+				}
+
+				const language = getLanguage();
+				const { read } = ctx;
+				async function readAndUpdateLanguage() {
+					const content = await read();
+					language.updateFile(file, content);
+
+					return content;
+				}
+
+				if (!filter(file)) {
+					await readAndUpdateLanguage();
+
+					return;
+				}
+
+				let cached: Promise<string> | undefined;
+				ctx.read = async () => {
+					cached ??= (async () => {
+						const code = await readAndUpdateLanguage();
+						const result = transform(code, file, resolvedOptions);
+
+						return result?.code ?? code;
+					})();
+
+					return cached;
+				};
+
+				for (const mod of ctx.modules) {
+					ctx.server.moduleGraph.invalidateModule(mod);
+				}
+
+				return ctx.modules;
+			},
+		},
 
 		buildStart() {
 			const resolvedOptions = resolveOptions(options);
